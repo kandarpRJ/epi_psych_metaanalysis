d752ff1d1cb21d8dfd8f411defb2b7804d735543
kandarpRJ <kandarpbioinfo@gmail.com>
2021-02-05 13:58:17 +0900

    Update heatmap.R

--- heatmap.R
+++ heatmap.R
@@ -1,19 +1,29 @@
 library(ComplexHeatmap)
+library(RColorBrewer)
 
 data<-read.table("MDD_mastersheet.csv", header = TRUE, row.names = NULL, sep = ",")
 
 data<-data[with (data, order(data$Brain.region, data$Subregion, data$Sex)),]
 mat<-data[,5:ncol(data)]
 
-ha = HeatmapAnnotation(df = data[,2:4], 
-                       col = list(Sex=c("F"="green", "M"="yellow", "Mix"="orange")))
+brcol<-brewer.pal(n=12, name = "Paired")
+brcol[13]<-brewer.pal(n=4, name = "Set3")
+names(brcol)<-unique (sort (data$Brain.region))
+
+sbrcol<-brewer.pal(n=9, name = "Set3")
+sbrcol[10]<-"black"
+names(sbrcol)<-unique(sort(data$Subregion))
+sbrcol["L5"]<-"#E7298A"
+
+ha = HeatmapAnnotation(df = data[,2:4], col = list(Brain.region = brcol, Subregion = sbrcol,
+                       Sex=c("F"="green", "M"="yellow", "Mix"="orange", "Unknown/NA"="black")))
 
 h<-Heatmap(as.matrix(t(mat)), cluster_rows = FALSE, cluster_columns = FALSE, top_annotation = ha, 
            column_labels = data$Study, heatmap_legend_param = list (title="Log2FC", direction="horizontal", title_position="topcenter"),
-           row_split = data.frame(c(rep("Writers", 11), rep("Readers", 9), rep("Erasers", 2))),
+           row_split = data.frame(c(rep("Writers", 7), rep("Readers", 9), rep("Erasers", 2))),
            column_split = data.frame(data$Brain.region), column_title = NULL)
 
-png("MDD_heatmap_with_biorxiv.png", res = 600, width = 5000, height = 5000)
+png("figure4.png", res = 600, width = 5000, height = 5000)
 draw(h, heatmap_legend_side="bottom")
 dev.off()
 
@@ -36,10 +46,9 @@ h<-Heatmap(as.matrix(t(mat)), cluster_rows = FALSE, cluster_columns = FALSE, top
            row_split = data.frame(c(rep("Writers", 11), rep("Readers", 9), rep("Erasers", 2))),
            column_split = data.frame(data$Study), column_title = NULL)
 
-png("PTSD_heatmap_with_biorxiv.png", res = 600, width = 5000, height = 5000)
+png("figure6.png", res = 600, width = 5000, height = 5000)
 draw(h, heatmap_legend_side="bottom")
 dev.off()
 
 
 #########################################################################################################################
-
